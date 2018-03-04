c4d95b6a77fb89ab6db9e4ec6c5a1d99742ced75
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-05 01:48:22 +0800

    test event coord

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-var stretch = require('stretch-canvas');
+var stretch = require('stretch-canvas').stretch;
+var unstretchCoordinates = require('stretch-canvas').unstretchCoordinates;
 var updateState = require('./update_state');
 var paintFrame = require('./paint_frame');
 
 var canvas = document.getElementById('ripple-canvas');
 var ctx = canvas.getContext('2d');
 
-var originalWidth = 100;
-var originalHeight = 100;
+var original_width = 100;
+var original_height = 100;
 
-canvas.width = originalWidth;
-canvas.height = originalHeight;
+canvas.width = original_width;
+canvas.height = original_height;
 
 stretch(canvas);
 
@@ -30,9 +31,15 @@ function mainCycle(timestamp) {
     var ms_elapsed = last_timestamp ? (timestamp - last_timestamp) : 0;
     last_timestamp = timestamp;
 
-    updateState(state, ms_elapsed, pixels_per_second, originalWidth, originalHeight);
+    updateState(state, ms_elapsed, pixels_per_second, original_width, original_height);
     paintFrame(ctx, state);
     window.requestAnimationFrame(mainCycle);
 }
 
 window.requestAnimationFrame(mainCycle);
+
+canvas.addEventListener('click', function(e) {
+    console.log(e);
+    console.log(e.offsetX, e.offsetY);
+    console.log(unstretchCoordinates(e.offsetX, e.offsetY));
+});
